8ef0d7034a05756e42bfd57f627a9bfcb3578519
Vitor <vitor.aguiar@me.com>
2016-02-03 10:54:04 -0200

    Create df2.R

---
+++ df2.R
@@ -0,0 +1,5 @@
+df2 <- read.table(text = "ind   sex control trt1    trt2
+        1   m   7.9 12.3    10.7
+        2   f   6.3 10.6    11.1
+        3   f   9.5 13.1    13.8
+        4   m   11.5    13.4    12.9", header = TRUE)
